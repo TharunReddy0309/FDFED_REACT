bf053d5567b358c823d7f11361b52d2e99eacdd1
TharunReddy0309 <bhumireddytharunreddy9@gmail.com>
2026-09-01 13:26:04 +0530

    Todays task for child parent communication

--- attendance/src/App.tsx
+++ attendance/src/App.tsx
@@ -1,27 +1,54 @@
 import { useState } from "react";
 import "./App.css";
+import profilePhoto from "./assets/hero.png";
+import ram from "./assets/ram.png";
+import sneha from "./assets/sneha.png";
+
+const profiles = [
+    { name: "Ram", age: 21, photo: ram , num: 10 },
+    { name: "Priya", age: 22, photo: profilePhoto , num :20 },
+    { name: "Aman", age: 20, photo: profilePhoto, num :30 },
+    { name: "Sneha", age: 23, photo: sneha , num :40 },
+];
 
 function App() {
 
-    // Parent owns the state
+    const [selectedProfile, setSelectedProfile] = useState(profiles[0]);
+
     const [totalStudents, setTotalStudents] = useState(30);
 
-    // Event handler in parent
-    function changeTotal() {
-        setTotalStudents(totalStudents + 1);
+    function changeTotal({x}: {x: number}) {
+        setTotalStudents(totalStudents + x);
     }
 
     return (
         <div className="container">
 
-            {/* Passing event handler to child */}
-            <Attendance
+            <Attendance totalStudents={totalStudents} />
+
+            {profiles.map((person) => (
+                <button
+                    key={person.name}
+                    onClick={() => setSelectedProfile(person)}
+                >
+                    {person.name}
+                </button>
+            ))}
+
+            <h2>Parent Total Students: {totalStudents}</h2>
+
+            <Greeting
+                name={selectedProfile.name}
                 totalStudents={totalStudents}
-                onChangeTotal={changeTotal}
+                changeTotal={changeTotal}
+                num={selectedProfile.num}
             />
 
-            {/* Passing total students to Greeting */}
-            <Greeting totalStudents={totalStudents} />
+            <Profile
+                name={selectedProfile.name}
+                age={selectedProfile.age}
+                photo={selectedProfile.photo}
+            />
 
         </div>
     );
@@ -32,16 +59,17 @@ function Attendance(props: any) {
 
     const [presentStudents, setPresentStudents] = useState(0);
 
+    const var1 = `Hello ${presentStudents}`;
+
     function markPresent() {
-
-        if (presentStudents < props.totalStudents) {
+        if (presentStudents < props.totalStudents)
             setPresentStudents(presentStudents + 1);
-        }
 
+        console.log(var1);
     }
 
     return (
-        <div>
+        <div className="container">
 
             <h1>Attendance Tracker</h1>
 
@@ -60,11 +88,6 @@ function Attendance(props: any) {
                 Mark Present
             </button>
 
-            {/* Child calls parent's event handler */}
-            <button onClick={props.onChangeTotal}>
-                Add Student
-            </button>
-
         </div>
     );
 }
@@ -74,14 +97,35 @@ function Greeting(props: any) {
 
     return (
         <div>
-            <h1>Hello!</h1>
 
-            <h2>
-                Total Students: {props.totalStudents}
-            </h2>
+            <h1>Hello, {props.name}!</h1>
+            <p>Total Students: {props.totalStudents}</p>
+            <button onClick={() => props.changeTotal({ x: props.num })}>
+                Change No. of Students
+            </button>
+
         </div>
     );
 }
 
+
+function Profile(props: any) {
+
+    return (
+        <div>
+
+            <img
+                src={props.photo}
+                alt={props.name}
+                width="150"
+            />
+
+            <h1>{props.name}</h1>
+
+            <p>Age: {props.age}</p>
+
+        </div>
+    );
+}
 
 export default App;
